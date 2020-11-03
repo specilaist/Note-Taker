95fc28339579da8245d60a584856a4885c1bd9c4
specilaist <specilaist@msn.com>
2020-11-02 18:38:13 -0800

    Routes work

--- Routes/api/notes/index.js
+++ Routes/api/notes/index.js
@@ -2,16 +2,18 @@ const router = require("express").Router();
 const fs = require("fs");
 let db = require("../../../db/db.json");
 const path = require("path");
-
+const { request } = require("http");
 
 router.get("/notes", function (req, res) {
+  console.log("This is the get request");
   res.json(db);
+  console.log(db);
 });
 
 router.post("/notes", function (req, res) {
-      console.log(req.body);
+  console.log(req.body);
   const newNote = req.body;
-  newNote.id = db.length
+  newNote.id = db.length;
   console.log(newNote);
   fs.readFile(path.join(__dirname, "../../../db/db.json"), "utf8", function (err, data) {
     if (err) {
@@ -21,34 +23,38 @@ router.post("/notes", function (req, res) {
     let newData = JSON.parse(data);
     newData.push(newNote);
     let jsonData = JSON.stringify(newData);
-    fs.writeFile(path.join(__dirname, "../../../db/db.json"), jsonData, function (err, data) {
-      if (err) console.log(err);
-      console.log("new");
-      res.json(newData) 
-    });
+    fs.writeFile(
+      path.join(__dirname, "../../../db/db.json"),
+      jsonData,
+      function (err, data) {
+        if (err) console.log(err);
+        console.log(data);
+        res.json(newData);
+      }
+    );
   });
 });
 
 router.delete("/notes/:id", function (req, res) {
-// //   res.json(data);
-//   console.log(req.body);
-//   const deletNote = req.body;
-//   fs.readFile(path.join(__dirname, '../../../db/db.json'), deletNote, function(err, data) {
-//       if (err) throw err;
-//       console.log(data);
-//       // const newDelete
-//   })
-// console.log(db);
-db = db.filter(function(note){
-      console.log(note.id);
-      console.log(req.params.id);
-      if (note.id === parseInt(req.params.id)) {
-            return false
-      } return true
-});
+  // //   res.json(data);
+  //   console.log(req.body);
+  //   const deletNote = req.body;
+  //   fs.readFile(path.join(__dirname, '../../../db/db.json'), deletNote, function(err, data) {
+  //       if (err) throw err;
+  //       console.log(data);
+  //       // const newDelete
+  //   })
+  // console.log(db);
+  db = db.filter(function (note) {
+    console.log(note.id);
+    console.log(req.params.id);
+    if (note.id === parseInt(req.params.id)) {
+      return false;
+    }
+    return true;
+  });
 
-res.json(db);
-
+  res.json(db);
 });
 
 module.exports = router;
